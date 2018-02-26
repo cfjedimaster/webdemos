85be62af28cc3d4246ac28ece3acd8de82837e8d
Raymond Camden <raymondcamden@gmail.com>
2018-02-26 17:26:13 -0600

    small tweak

--- happy/main.js
+++ happy/main.js
@@ -30,17 +30,9 @@ const app = new Vue({
 	}
 });
 
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
-
-/*
-setInterval(() => {
-	app.text = (new Date()).toString();
-	app.loading='';
-  }, 3000);
-*/
