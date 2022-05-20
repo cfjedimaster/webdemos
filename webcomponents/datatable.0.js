d71b8f9360508f2e7b5a29e802fecc36117d8f41
Raymond Camden <raymondcamden@gmail.com>
2022-05-20 14:32:56 -0500

    stuff

---
+++ webcomponents/datatable.0.js
@@ -0,0 +1,117 @@
+class DataTable extends HTMLElement {
+
+
+	
+
+	constructor() {
+		super();
+
+
+		if(this.hasAttribute('src')) this.src = this.getAttribute('src');
+		// If no source, do nothing
+		if(!this.src) return;
+
+		// attributes to do, datakey + cols
+		if(this.hasAttribute('cols')) this.cols = this.getAttribute('cols').split(',');
+
+		this.sortAsc = false;
+
+        const shadow = this.attachShadow({
+            mode: 'open'
+        });
+		
+        const wrapper = document.createElement('table');
+		const thead = document.createElement('thead');
+		const tbody = document.createElement('tbody');
+		wrapper.append(thead, tbody);
+        shadow.appendChild(wrapper);
+
+        const style = document.createElement('style');
+        style.textContent = `
+table { 
+	border-collapse: collapse;
+}
+
+td, th {
+  padding: 5px;
+  border: 1px solid black;
+}
+    `;
+        // Attach the created elements to the shadow dom
+        shadow.appendChild(style);
+
+		// https://www.freecodecamp.org/news/this-is-why-we-need-to-bind-event-handlers-in-class-components-in-react-f7ea1a6f93eb/
+		this.sort = this.sort.bind(this);
+
+
+	}
+
+	async load() {
+		console.log('load', this.src);
+		// error handling needs to be done :|
+		let result = await fetch(this.src);
+		this.data = await result.json();
+		this.render();
+	}
+
+	async render() {
+		console.log('render time', this.data);
+		if(!this.cols) this.cols = Object.keys(this.data[0]);
+
+		// header
+		let header = '<tr>';
+		this.cols.forEach(col => {
+			header += `<th data-sort="${col}">${col}</th>`;
+		});
+		let thead = this.shadowRoot.querySelector('thead');
+		thead.innerHTML = header;
+
+		// body
+		let result = '';
+		this.data.forEach(c => {
+			let r = '<tr>';
+			this.cols.forEach(col => {
+				r += `<td>${c[col]}</td>`;
+			});
+			r += '</tr>';
+			result += r;
+		});
+
+		let tbody = this.shadowRoot.querySelector('tbody');
+		tbody.innerHTML = result;
+
+		this.shadowRoot.querySelectorAll('thead tr th').forEach(t => {
+			t.addEventListener('click', this.sort, false);
+		});
+
+	}
+
+	async sort(e) {
+		let thisSort = e.target.dataset.sort;
+		console.log('sort by',thisSort);
+
+		if(this.sortCol && this.sortCol === thisSort) this.sortAsc = !this.sortAsc;
+		console.log('dir is ', this.sortAsc);
+		this.sortCol = thisSort;
+		this.data.sort((a, b) => {
+			if(a[this.sortCol] < b[this.sortCol]) return this.sortAsc?1:-1;
+			if(a[this.sortCol] > b[this.sortCol]) return this.sortAsc?-1:1;
+			return 0;
+		});
+		this.render();	
+	}
+
+	static get observedAttributes() { return ['src']; }
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		// even though we only listen to src, be sure
+		if(name === 'src') {
+			this.src = newValue;
+			this.load();
+		}
+	}
+
+}
+
+// Define the new element
+customElements.define('data-table', DataTable);
