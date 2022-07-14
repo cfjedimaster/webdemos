8be5ac195d5a524e5f2abc65f1e638c303b5b696
Raymond Camden <raymondcamden@gmail.com>
2022-07-14 09:08:39 -0500

    first commit

---
+++ tweetimages2/app.js
@@ -0,0 +1,41 @@
+
+let $status, $searchForm, $searchButton, $twitterAccount, $results;
+
+const endpoint = 'https://eo73fogw8n2gknw.m.pipedream.net';
+
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init() {
+	$status = document.querySelector('#statusArea');
+	$searchForm = document.querySelector('#searchForm');
+	$searchButton = document.querySelector('#searchButton');
+	$twitterAccount = document.querySelector('#twitterAccount');
+	$results = document.querySelector('#results');
+
+	$searchButton.addEventListener('click', search);
+	$searchForm.style.display = '';
+
+}
+
+async function search() {
+	let account = $twitterAccount.value.trim();
+	if(!account) return;
+	if(account.indexOf('@') === 0) account = account.replace('@','');
+	$results.style.display = 'none';
+	$status.innerText = `Finding images for the account, ${account}.`;
+	let resp = await fetch(endpoint + `?account=${encodeURIComponent(account)}`);
+	let data = await resp.json();
+	let html = '';
+
+	data.forEach(i => {
+		html += `
+<a href="${i}" class="lightbox"><img src="${i}"></a>
+		`;
+	});
+
+	$status.style.display = 'none';
+	$results.innerHTML = html;
+	$results.style.display = '';
+	const prvs = new Parvus();
+
+}
