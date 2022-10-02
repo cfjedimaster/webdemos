0eac2bee2427950f120873dc01d43ec984802385
Raymond Camden <raymondcamden@gmail.com>
2022-10-02 13:12:46 -0500

    misc

--- webcomponents/pdfembed.js
+++ webcomponents/pdfembed.js
@@ -1,22 +1,28 @@
+ import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 
-class PDFEmbed extends HTMLDivElement {
+class PDFEmbed extends HTMLElement {
 
 
     constructor() {
 
         super();
 
-        const shadow = this.attachShadow({
-            mode: 'open'
-        });
-		
-//        const wrapper = document.createElement('div');
+		this.divid = uuidv4();
 
-//		wrapper.id = 'temp';
+        const wrapper = document.createElement('div');
+		wrapper.id = this.divid;
 
-		if(this.hasAttribute('url')) this.url = this.getAttribute('url');
-		
+		if(this.hasAttribute('url')) this.url = this.getAttribute('url');		
 		if(this.hasAttribute('key')) this.key = this.getAttribute('key');
+
+		if(this.hasAttribute('width')) this.width = this.getAttribute('width');
+		else this.width = '500px';
+
+		if(this.hasAttribute('height')) this.height = this.getAttribute('height');
+		else this.height = '500px';
+
+		this.embedMode = 'FULL_WINDOW';
+		if(this.hasAttributes('embedMode')) this.embedMode = this.getAttribute('embedMode');
 
 		// if no url, safe to just return?
 		if(!this.url) {
@@ -30,37 +36,34 @@ class PDFEmbed extends HTMLDivElement {
 			return;
 		}
 
-        const style = document.createElement('style');
-        style.textContent = `
-div {
-	width: 500px;
-	height: 500px;
-}
-    `;
-        shadow.appendChild(style);
+		this.name = this.url.split('/').pop();
 
-
-        //shadow.appendChild(wrapper);
+		wrapper.style = `width: ${this.width}; height: ${this.height}`;
+		this.parentNode.insertBefore(wrapper, this.nextSibling);
 
 	}
 
 	loadPDF() {
-		console.log('loadPDF', this.url, this.key);
-		var adobeDCView = new AdobeDC.View({clientId: this.key, divId: "temp"});
+		var adobeDCView = new AdobeDC.View({clientId: this.key, divId: this.divid });
 		adobeDCView.previewFile({
-			content:{location: {url: "https://documentcloud.adobe.com/view-sdk-demo/PDFs/Bodea%20Brochure.pdf"}},
-			metaData:{fileName: "Bodea Brochure.pdf"}
-		}, {embedMode: "SIZED_CONTAINER"});	
+			content:{location: {url: this.url }},
+			metaData:{fileName: this.name}
+		}, {embedMode: this.embedMode});	
 
 	}
 
 	connectedCallback() {
-		console.log('connected callback');
 
-		// todo, only add once
+		/*
+		the below doesnt work in my test where I have 2 right after each other, but it makes
+		sense, we haven't loaded yet. However, if I use JS to add a new pdf-embed element, in theory, 
+		this optimization will work.
+		*/
+		if(window.AdobeDC) { this.loadPDF(); return; }
+
 		const script = document.createElement('script');
 		script.type = 'text/javascript';
-		script.src='https://documentcloud.adobe.com/view-sdk/main.js';
+		script.src='https://documentservices.adobe.com/view-sdk/viewer.js';
 		document.head.appendChild(script);
 
 		if(window.AdobeDC) this.loadPDF();
@@ -70,5 +73,4 @@ div {
 
 }
 
-// Define the new element
-customElements.define('pdf-embed', PDFEmbed, { extends:'div' });
+customElements.define('pdf-embed', PDFEmbed);
