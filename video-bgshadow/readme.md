ba77f95d1e2fe1e50214745e3cb36e793ad54d99
Raymond Camden <raymondcamden@gmail.com>
2026-04-30 08:15:59 -0500

    new dmeo

---
+++ video-bgshadow/readme.md
@@ -0,0 +1,7 @@
+# <video-bgshadow>
+
+This is a simple web component that uses the excellent [ColorThief](https://lokeshdhakar.com/projects/color-thief/) library to add an animated background to videos that match their dominent color. It's based on the example they created here: <https://lokeshdhakar.com/projects/color-thief/#v3-observe>
+
+Right now it doesn't support any arguments, but I could see adding values for the size of the shadow. 
+
+You can run the demo here: <https://cfjedimaster.github.io/webdemos/video-bgshadow/>
--- video-bgshadow/script.js
+++ video-bgshadow/script.js
@@ -4,32 +4,39 @@ class VideoBGShadowComponent extends HTMLElement {
 		super();
 	}
 	
-	connectedCallback() {
-		console.log('connected like a g6');
+	async connectedCallback() {
 		this.videoEl = this.querySelector('video');
 		if(!this.videoEl) {
 			console.warn('No <video> element found.');
 			return;
 		}
 
-        console.log('src', this.videoEl.querySelector('source').src);
 		// wrap the video in a new div
 		this.wrapper = document.createElement('div');
 		this.videoEl.parentNode.insertBefore(this.wrapper, this.videoEl);
 		this.wrapper.appendChild(this.videoEl);
 		this.wrapper.style.display = 'inline-block';
 		this.videoEl.style.verticalAlign = 'bottom';
+		if(!window.ColorThief) await this.loadCF();
+		this.videoEl.addEventListener('play', this.startShadow.bind(this));
+		this.videoEl.addEventListener('ended', this.endShadow.bind(this));
+		this.videoEl.addEventListener('pause', this.endShadow.bind(this));
 
-		const script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.src='https://unpkg.com/colorthief@3/dist/umd/color-thief.global.js';
-		document.head.appendChild(script);
-		script.onload = () => {
-			this.videoEl.addEventListener('play', this.startShadow.bind(this));
-			this.videoEl.addEventListener('ended', this.endShadow.bind(this));
-			this.videoEl.addEventListener('pause', this.endShadow.bind(this));
 	
+	}
+
+	// Sets window.ColorThiefLoading (Promise) to deduplicate concurrent script injection across multiple instances.
+	async loadCF() {
+		if (!window.ColorThiefLoading) {
+			window.ColorThiefLoading = new Promise((resolve) => {
+				const script = document.createElement('script');
+				script.type = 'text/javascript';
+				script.src = 'https://unpkg.com/colorthief@3/dist/umd/color-thief.global.js';
+				document.head.appendChild(script);
+				script.onload = resolve;
+			});
 		}
+		return window.ColorThiefLoading;
 	}
 
 	startShadow(e) {
